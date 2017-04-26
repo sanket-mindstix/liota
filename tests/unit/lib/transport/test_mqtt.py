e7cd33e65f5f9966cf3aa703be699f1ab8855a9e
Sanket Kulkarni <sanket.kulkarni@mindstix.com>
2017-04-26 11:27:40 +0530

    Updated Mqtt

--- tests/unit/lib/transport/test_mqtt.py
+++ tests/unit/lib/transport/test_mqtt.py
@@ -754,7 +754,7 @@ class QoSDetailsTest(unittest.TestCase):
         # Check value of queue_size
         self.assertEqual(qos_details.queue_size, self.queue_size, "Invalid implementation for QoSDetails class")
         
-        # self.assertTrue(False)
+        self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main(verbosity=1)
